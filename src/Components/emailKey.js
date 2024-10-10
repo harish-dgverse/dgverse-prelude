c7abe2b0d5a0530b8be56a9bceb0f50f589a0d15
Harish Haridas <harish.haridas@dgverse.in>
2024-10-10 10:27:44 +0530

    completed styling

--- src/Components/emailKey.js
+++ src/Components/emailKey.js
@@ -1,5 +1,5 @@
 export default {
-    USER_ID: `oI4mfiTrVy4YR7kZd`, //public key
-    TEMPLATE_ID: `template_osxrf3h`,         //templateID
-    SERVICE_ID: `service_7uveirr`,
+    USER_ID: `user_id`, //public key
+    TEMPLATE_ID: `template_id`,         //templateID
+    SERVICE_ID: `service_id`,
 }
